614a6b897172269ba7d5433c5e60fdf49a2ef7b1
jackal911 <thisisdiss@gmail.com>
2022-06-22 23:14:10 +0900

    c언어 tcp스쿨 상수 단원까지

--- TCPSchool_C/main.c
+++ TCPSchool_C/main.c
@@ -1,13 +1,51 @@
 #include <stdio.h>
 
 int main(void) {
+/*
+
+// 1~6) printf()와 scanf() :
 	int num01, num02;
 	
-	printf("첫 번째 정수를 입력하세요 : ");
-	scanf("%d", &num01);
-	printf("두 번째 정수를 입력하세요 : ");
-	scanf("%d", &num02);
+	printf("두 개의 정수를 입력하세요 : ");
+	scanf("%d %d", &num01, &num02);
 	
-	printf("입력하신 두 정수의 합은 %d입니다.\n", num01 + num02);
-	return 0;
+	printf("입력하신 두 정수를 8진수로 나타내면 %o와 %o가 되고,\n", num01, num02);
+	printf("입력하신 두 정수를 16진수로 나타내면 %x와 %x가 되고,\n", num01, num02);
+
+
+	float num01;
+	double num02;
+	
+	printf("두 실수를 입력하세요 : ");
+	scanf("%f %f", &num01, &num02);
+	printf("입력받은 두 실수는 %f, %f입니다.\n", num01, num02);
+	printf("두 실수 꼬라지가 이상하죠?\n\n");
+	
+	printf("한번더 입력해봅시다 : ");
+	scanf("%f %lf", &num01, &num02);
+	printf("입력받은 두 실수는 %f, %f입니다.\n", num01, num02);
+	printf("이번에는 두 실수를 제대로 저장했네요.\n");
+
+// 7) variable :
+	int ss;
+	printf("%d", ss); // 0 출력 
+	
+	int num01, num02;
+	double num03 = 1.23, num04 = 4.56;
+
+
+// 8) constant :
+	// 리터럴 상수(literal constant) :  
+	123;
+	3.14;
+	'a';
+	
+	// 심볼릭 상수(symbolic constant) :
+	const int MAX = 10; // const 키워드를 이용한 심볼릭 상수 
+	#define MAX 10; // #define 선행처리 지시자를 이용한 매크로 심볼릭 상수
+	
+*/
+
+// 9) Primitive Type :
+	 
 }
